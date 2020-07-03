0267011a5ce385f78556fa7985a398ca5cc9cc96
Seyi-Baks <oluwaseyi.bakare@gmail.com>
2020-07-03 23:07:42 +0100

    Redis impl

--- config/redis_db.js
+++ config/redis_db.js
@@ -2,6 +2,8 @@ const redis = require('redis');
 
 const { REDIS_PORT = 6379 } = process.env;
 
-const redis_client = redis.createClient(REDIS_PORT);
+const redis_client = redis.createClient(process.env.REDISCLOUD_URL, {
+  no_ready_check: true,
+});
 
 module.exports = redis_client;
